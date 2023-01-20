2ff92cdcbb061da95206a5ac1e1594ad8d2bd803
leonardo <leeo_skw@hotmail.com>
2023-01-20 00:07:32 -0300

    implementation of "GET" and "GET by ID" methods

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -3,10 +3,9 @@ package com.example.demo.controllers;
 import com.example.demo.models.entities.Product;
 import com.example.demo.models.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -27,6 +26,16 @@ public class ProductController {
         productRepository.save(product);
         return product;
     }
-
     // as duas formas acima funcionam: seja recebendo cada atributo individualmente, ou recebendo o próprio objeto completo.
+
+    @GetMapping()
+    public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable int id) {
+        return productRepository.findById(id);
+        // como é uma variável que está presente dentro da URL, utilizamos @PathVariable
+    }
 }
